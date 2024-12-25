b301490b7ca4a15ae50d39975fa258dba8b41192
Yves Donato <draonzilla9@gmail.com>
2024-12-25 13:05:04 -0500

    adjusted css

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 </script>
 
-<nav class="bg-white rounded-lg shadow my-4 text-gray-800 bg-gray-800">
+<nav class="rounded-lg shadow my-4 text-gray-800 bg-gray-800">
     <div class="container flex items-center justify-center p-6 mx-auto capitalize text-gray-300">
         <a href="/" class="border-b-2 transition-colors duration-300 transform hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
         						class:border-blue-500={$page.url.pathname === '/'}
