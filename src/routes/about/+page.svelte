6b08481d9d68f56a90f37cc57a0f8d5c0d7654b2
Yves Donato <draonzilla9@gmail.com>
2024-12-26 17:43:39 -0500

    added about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,37 @@
-<div class="rounded-lg shadow p-6 text-gray-300 bg-gray-800">
-  <h1 class="text-center">There is nothing here yet.</h1>
+<div class="rounded-lg shadow p-6 my-10 text-gray-300 bg-gray-800">
+  <h1 class="text-xl text-center mb-5">Bio</h1>
+  <hr>
+  <p class="my-5">
+  Name: Yves Donato <br>
+  Contact: yvesdonato0@gmail.com <br>
+  </p>
+  <h1 class="text-xl text-center mb-5">Education</h1>
+  <hr>
+  <p class="mt-5">
+  School: Sheridan College <br>
+  Current Program: Honours Bachelor of Computer Science <br>
+  Expected Graduation: Spring 2026 <br><br>
+  School: Sheridan College <br>
+  Graduated Program: Computer Programming <br>
+  Graduated Date: Spring 2023 <br><br>
+  School: St. Mary Catholic Secondary School <br>
+  SHSM: Energy (Computer Science), Now called Information and Communications Technology (2022) <br>
+  Graduated Date: Summer 2021 <br>
+  </p>
+  <h1 class="text-xl text-center mb-5">Work Experience</h1>
+  <hr>
+  <p class="mt-5">
+  Current Company: Walmart Canada <br>
+  Occupation Title: Dairy Frozen Associate <br>
+  Start Date: 2022 <br>
+  Company: Weever Apps  <br><br>
+  Occupation: QA Intern <br>
+  Start Date: Summer 2018 <br>
+  End Date: Winter 2018 <br><br>
+  Company: Reliq Health <br>
+  Occupation: Software Developer Intern <br>
+  Start Date: Fall 2018 <br>
+  End Date: Winter 2019 <br>
+  </p>
+
 </div>
