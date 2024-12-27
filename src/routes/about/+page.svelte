c7ec3eec8f30f3fa169a5781a8c1abfcde9ec811
Yves Donato <draonzilla9@gmail.com>
2024-12-27 10:47:39 -0500

    Updated Look of About Page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,37 +1,72 @@
 <div class="rounded-lg shadow p-6 my-10 text-gray-300 bg-gray-800">
   <h1 class="text-xl text-center mb-5">Bio</h1>
   <hr>
-  <p class="my-5">
-  Name: Yves Donato <br>
-  Contact: yvesdonato0@gmail.com <br>
+  <p class="my-5 font-semibold text-white">
+    Name: Yves Donato <br>
+    Contact: yvesdonato0@gmail.com <br>
   </p>
+</div>
+<div class="rounded-lg shadow p-6 my-10 text-gray-300 bg-gray-800">
   <h1 class="text-xl text-center my-5">Education</h1>
   <hr>
-  <p class="mt-5">
-  School: Sheridan College <br>
-  Current Program: Honours Bachelor of Computer Science <br>
-  Expected Graduation: Spring 2026 <br><br>
-  School: Sheridan College <br>
-  Graduated Program: Computer Programming <br>
-  Graduated Date: Spring 2023 <br><br>
-  School: St. Mary Catholic Secondary School <br>
-  SHSM: Energy (Computer Science), Now called Information and Communications Technology (2022) <br>
-  Graduated Date: Summer 2021 <br>
-  </p>
+  <ol class="relative border-s border-gray-700 mt-5">                  
+      <li class="mb-10 ms-4">
+          <div class="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-500">Spring 2026</time>
+          <h3 class="text-lg font-semibold text-white">Honours Bachelor of Computer Science</h3>
+          <p class="mb-4 text-base font-normal text-gray-400">
+            Honours Bacelors of Computer Science Degree Completion at Sheridan College.
+          </p>
+          <a href="https://www.sheridancollege.ca/programs/bachelor-computer-science#tab=overview" class="inline-flex items-center px-4 py-2 text-sm font-medium border rounded-lg focus:z-10 focus:ring-4 focus:outline-none focus:text-blue-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 focus:ring-gray-700">Learn more <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+          </a>
+      </li>
+      <li class="mb-10 ms-4">
+          <div class="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-500">Spring 2023</time>
+          <h3 class="text-lg font-semibold text-white">Computer Programming</h3>
+          <p class="mb-4 text-base font-normal text-gray-400">
+            Alumni of Sheridan College Institute of Technology and Advanced Learning.
+          </p>
+      </li>
+      <li class="mb-10 ms-4">
+          <div class="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-500">Summer 2021</time>
+          <h3 class="text-lg font-semibold text-white">High School SHSM Computer Science</h3>
+          <p class="mb-4 text-base font-normal text-gray-400">
+            High school education at St. Mary Catholic Secondary School. Graduated with Specialist High Skills Major Certification, Majored in Computer Science formerly known as Energy, now called Information and Communications Technology (2022). 
+          </p>
+      </li>
+  </ol>
+</div>
+<div class="rounded-lg shadow p-6 my-10 text-gray-300 bg-gray-800">
   <h1 class="text-xl text-center my-5">Work Experience</h1>
   <hr>
-  <p class="mt-5">
-  Current Company: Walmart Canada <br>
-  Occupation Title: Dairy Frozen Associate <br>
-  Start Date: 2022 <br><br>
-  Company: Weever Apps  <br>
-  Occupation: QA Intern <br>
-  Start Date: Summer 2018 <br>
-  End Date: Winter 2018 <br><br>
-  Company: Reliq Health <br>
-  Occupation: Software Developer Intern <br>
-  Start Date: Winter 2018 <br>
-  End Date: Spring 2019 <br>
-  </p>
-
+  <ol class="relative border-s border-gray-700 mt-5">                  
+      <li class="mb-10 ms-4">
+          <div class="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-500">Spring 2022</time>
+          <h3 class="text-lg font-semibold text-white">Walmart Canada</h3>
+          <p class="mb-4 text-base font-normal text-gray-400">
+            Part-time Job while doing my Computer Science Degree, Dairy Frozen Associate.
+          </p>
+      </li>
+      <li class="mb-10 ms-4">
+          <div class="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-500">Winter 2019</time>
+          <h3 class="text-lg font-semibold text-white">Reliq Health</h3>
+          <p class="mb-4 text-base font-normal text-gray-400">
+            COOP, Software Devloper Intern.
+          </p>
+      </li>
+      <li class="mb-10 ms-4">
+          <div class="absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-gray-900 bg-gray-700"></div>
+          <time class="mb-1 text-sm font-normal leading-none text-gray-500">Summer 2018</time>
+          <h3 class="text-lg font-semibold text-white">Weever Apps</h3>
+          <p class="mb-4 text-base font-normal text-gray-400">
+            COOP, QA (Quality Assurance) Intern.
+          </p>
+      </li>
+  </ol>
 </div>
