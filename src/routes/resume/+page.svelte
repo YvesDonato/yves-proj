76ff8f48444cd202855ae89837c625019746c098
YvesDonato <yvesdonato0@gmail.com>
2026-05-13 22:58:37 -0400

    Feat: change resume

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -17,43 +17,7 @@
 					<p>
 						I want to formally thank you for taking your time to both examine my resume and website. I
 						know there are plenty of exceptional applicants who have applied to your company, so taking
-						your time to click on the link on my resume means the world to me, even if you were just
-						checking if I was lying about having a website.
-					</p>
-
-					<p>
-						I&apos;m sure that you were expecting a letter explaining how fantastic I am and how I know
-						everything about computer science and IT, but this is not true; virtually no one at the
-						CO-OP or intern stage of their professional experience does. What I can say for sure is that
-						in the small sections of computer science I have chosen to specialize in, I have a depth of
-						knowledge that many other applicants do not possess. I hope that I can prove to you that this
-						is the case.
-					</p>
-
-					<p>
-						One more point and I&apos;ll end my spiel. I truly love computer science; I love building
-						systems and infrastructure, even if no one will ever see them. I&apos;ve cut a ton of
-						personal projects from my resume; this website is technically one of them. Every summer since
-						I learned to code was filled with coding projects. I code so much I no longer think of it as
-						work; it is just a way of life for me.
-					</p>
-
-					<p>
-						In my case, it literally has to be; every time I want to add a program to my computer, I
-						have to hard-code it into my system. That&apos;s how
-						<a
-							href="https://nixos.org/"
-							target="_blank"
-							rel="noopener noreferrer"
-							class="text-[var(--terminal-primary)] underline decoration-[var(--terminal-primary)] underline-offset-3"
-						>
-							NixOS
-						</a>
-						works.
-					</p>
-
-					<p>
-						Spiel over, thank you for reading this and examining both my resume and website.
+						your time to click on the link on my resume means the world to me, thank you!
 					</p>
 
 					<p>
